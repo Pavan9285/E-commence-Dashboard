d69797cb65ce88c4a190ceaba4a063f64387c3e4
pavan.gupta@algorisys.com <pavan.gupta@algorisys.com>
2022-03-26 15:41:16 +0530

    added form validation and error handling to signup page

--- front-end/src/components/SignUp.js
+++ front-end/src/components/SignUp.js
@@ -8,7 +8,8 @@ export default function SignUp() {
         password: "",
     });
 
-    
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -20,28 +21,33 @@ export default function SignUp() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        // console.log(registerData)
-        let result = await fetch('http://localhost:5000/register', {
-            method: 'post',
-            body: JSON.stringify({
-                username: registerData.username,
-                email: registerData.email,
-                password: registerData.password
-            }),
-            headers: {
-                "Content-Type": "application/json",
-            }
-        });
-        result = await result.json();
-        localStorage.setItem("user", JSON.stringify(result));
+        setFormErrors(validate(registerData));
+        setIsSubmit(true);
 
-        // reset the form
-        setRegisterData({
-            username: "",
-            email: "Technology@gmail.com",
-            password: "",
-        });
-        navigate("/");
+        if (Object.keys(formErrors).length === 0 && isSubmit) {
+            // console.log(registerData)
+            let result = await fetch('http://localhost:5000/register', {
+                method: 'post',
+                body: JSON.stringify({
+                    username: registerData.username,
+                    email: registerData.email,
+                    password: registerData.password
+                }),
+                headers: {
+                    "Content-Type": "application/json",
+                }
+            });
+            result = await result.json();
+            localStorage.setItem("user", JSON.stringify(result));
+
+            // reset the form
+            setRegisterData({
+                username: "",
+                email: "Technology@gmail.com",
+                password: "",
+            });
+            navigate("/");
+        }
     }
 
     const handleChange = (e) => {
@@ -51,20 +57,60 @@ export default function SignUp() {
         })
     }
 
+    const validate = (values) => {
+        const errors = {}
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        const cond1 = /^(?=.*[a-z]).{4,10}$/;
+        const cond2 = /^(?=.*[A-Z]).{4,10}$/;
+        const cond3 = /^(?=.*[0-9]).{4,10}$/;
+
+        if (!values.username) {
+            errors.username = "Username is required";
+        }
+
+        if (!values.email) {
+            errors.email = "Email is required";
+        } else if (!regex.test(values.email)) {
+            errors.email = "This is not a valid email format!"
+        }
+
+        if (!values.password) {
+            errors.password = "Password is required";
+        } else if (values.password.length < 4) {
+            errors.password = "Password must be more than 4 characters"
+        } else if (values.password.length > 10) {
+            errors.password = "Password cannot exceed more than 10 characters"
+        } else if (!cond1.test(values.password)) {
+            errors.password = 'Password must contain at least one lowercase';
+        } else if (!cond2.test(values.password)) {
+            errors.password = 'Password must contain at least one capital letter';
+        } else if (!cond3.test(values.password)) {
+            errors.password = 'Password must contain at least a number';
+        }
+        return errors;
+    }
+
     return (
         <div className="register">
             {/* <pre>{JSON.stringify(registerData,undefined,2)}</pre> */}
             <form onSubmit={handleSubmit}>
                 <h1 style={{ color: "blue", margin: "20px" }}>Register</h1>
+
                 <input className="inputBox" type="text" placeholder="Enter Name"
                     name="username" value={registerData.username}
                     onChange={handleChange} />
+                <p style={{ color: "red" }}>{formErrors.username}</p>
+
                 <input className="inputBox" type="text" placeholder="Enter Email"
                     name="email" value={registerData.email}
                     onChange={handleChange} />
+                <p style={{ color: "red" }}>{formErrors.email}</p>
+
                 <input className="inputBox" type="password" placeholder="Enter Password"
                     name="password" value={registerData.password}
                     onChange={handleChange} />
+                <p style={{ color: "red" }}>{formErrors.password}</p>
+
                 <button type="submit" className="appButton">SignUp</button>
             </form>
         </div>
