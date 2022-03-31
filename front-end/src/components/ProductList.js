4b11a98336fbda7acf0bb34c19b8163041dd88f8
pavan.gupta@algorisys.com <pavan.gupta@algorisys.com>
2022-03-31 22:33:07 +0530

    completed frontend for delete product

--- front-end/src/components/ProductList.js
+++ front-end/src/components/ProductList.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
+let getRoute = 'http://localhost:5000/products';
+let deleteRoute = 'http://localhost:5000/product';
+
 const ProductList = () => {
     const [products, setProducts] = useState([]);
 
@@ -8,11 +11,28 @@ const ProductList = () => {
     }, []);
 
     const getProducts = async () => {
-        let result = await fetch('http://localhost:5000/products');
+        let result = await fetch(getRoute);
         // result in read stream formate so we called result.json()
         result = await result.json();
         setProducts(result);
-    }
+    };
+
+    const deleteProduct = (id) => {
+        const isDelete = window.confirm(`Are you sure you want to delete Product?`);
+        if (isDelete) {
+            deleteProductApiCall(id)
+        }
+    };
+
+    const deleteProductApiCall = async (id) => {
+        let result = await fetch(`${deleteRoute}/${id}`, {
+            method: "delete"
+        });
+        result = await result.json();
+        if (result) {
+            getProducts();
+        }
+    };
 
     return (
         <div className='product-list'>
@@ -23,15 +43,17 @@ const ProductList = () => {
                 <li>Price</li>
                 <li>Category</li>
                 <li>Company</li>
+                <li>Operation</li>
             </ul>
             {
                 products.map((item, index) =>
-                    <ul>
+                    <ul key={item._id}>
                         <li>{index + 1}</li>
                         <li>{item.name}</li>
                         <li>{item.price}</li>
                         <li>{item.category}</li>
                         <li>{item.company}</li>
+                        <li><button onClick={() => deleteProduct(item._id)}>Delete</button></li>
                     </ul>
                 )
             }
